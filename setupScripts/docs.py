8107dc8dc1b17b3d4c90591a1f76fa56c26f64cd
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-07-06 18:02:14 -0700

    Move test early

--- setupScripts/docs.py
+++ setupScripts/docs.py
@@ -4,13 +4,13 @@ from .cookdocs import CookDocs
 
 class Docs:
 	def __init__(self) -> None:
+		# GitHub specific commands only
+		print(f"GitHub Actions is: {os.getenv('GITHUB_ACTIONS')}")
 		CookLang()
 		CookDocs()
 		# Cloudflare specific commands only
 		if (os.getenv('CF_PAGES') != None and int(os.getenv('CF_PAGES')) == 1):
 			self.runMkDocs()
-		# GitHub specific commands only
-		print(f"GitHub Actions is: {os.getenv('GITHUB_ACTIONS')}")
 	
 	def runMkDocs(self) -> None:
 		runDocsAttempt = os.system('mkdocs build')
