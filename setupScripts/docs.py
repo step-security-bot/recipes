85578b12ee4fa87ed672b1237434c92c8d07a459
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-07-06 19:16:21 -0700

    Simplify generate command

--- setupScripts/docs.py
+++ setupScripts/docs.py
@@ -22,10 +22,10 @@ class Docs:
 	
 	def generate(self) -> None:
 		if (os.getenv('GITHUB_ACTIONS') != None and bool(os.getenv('GITHUB_ACTIONS')) == True):
-			runDocsAttempt = os.system('mkdocs gh-deploy')
-			if runDocsAttempt != 0:
-				raise Exception(f"mkdocs exited with code: {runDocsAttempt}")
+			subcommand = 'gh-deploy'
 		elif (os.getenv('CF_PAGES') != None and int(os.getenv('CF_PAGES')) == 1):
-			runDocsAttempt = os.system('mkdocs build')
-			if runDocsAttempt != 0:
-				raise Exception(f"mkdocs exited with code: {runDocsAttempt}")
+			subcommand = 'build'
+		
+		runDocsAttempt = os.system(f"mkdocs {subcommand}")
+		if runDocsAttempt != 0:
+			raise Exception(f"mkdocs exited with code: {runDocsAttempt}")
