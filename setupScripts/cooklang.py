5d8984c1989f6d18ed49d5c28c79bdcdce95b775
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-07-06 18:17:15 -0700

    Proper splitting into functions

--- setupScripts/cooklang.py
+++ setupScripts/cooklang.py
@@ -5,16 +5,22 @@ from pathlib import Path
 
 class CookLang:
 	def __init__(self) -> None:
-		self.download()
+		self.download(self.getDownloadLink())
+		self.unzip()
 
-	def download(self) -> None:
+	def getDownloadLink(self) -> str:
 		ghAssets = GhApi().repos.get_latest_release('cooklang', 'CookCLI').assets
 		for ghAsset in ghAssets:
 			if re.search('CookCLI_\d+\.\d+\.\d+_linux_amd64\.zip', ghAsset.name):
-				fileName = Path(ghAsset.browser_download_url).name
-				downloadAttempt = os.system(f"wget -nv {ghAsset.browser_download_url} -O {fileName}")
-				if downloadAttempt != 0:
-					raise Exception(f"wget exited with code: {downloadAttempt}")
-				unzipAttempt = os.system(f"unzip -o {fileName}")
-				if unzipAttempt != 0:
-					raise Exception(f"unzip exited with code: {unzipAttempt}")
+				return ghAsset.browser_download_url
+	
+	def download(self, downloadUrl:str) -> None:
+		self.zipFileName = Path(downloadUrl).name
+		downloadAttempt = os.system(f"wget -nv {downloadUrl} -O {self.zipFileName}")
+		if downloadAttempt != 0:
+			raise Exception(f"wget exited with code: {downloadAttempt}")
+	
+	def unzip(self) -> None:
+		unzipAttempt = os.system(f"unzip -o {self.zipFileName}")
+		if unzipAttempt != 0:
+			raise Exception(f"unzip exited with code: {unzipAttempt}")
