6e2cc2d6ad32f78559e9e6aac29aaa8708065064
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-07-06 18:30:04 -0700

    Actually run

--- setupScripts/docs.py
+++ setupScripts/docs.py
@@ -10,7 +10,7 @@ class Docs:
 	
 	def generate(self) -> None:
 		if (os.getenv('GITHUB_ACTIONS') != None and bool(os.getenv('GITHUB_ACTIONS')) == True):
-			runDocsAttempt = os.system('mkdocs gh-deploy --help')
+			runDocsAttempt = os.system('mkdocs gh-deploy')
 			if runDocsAttempt != 0:
 				raise Exception(f"mkdocs exited with code: {runDocsAttempt}")
 		elif (os.getenv('CF_PAGES') != None and int(os.getenv('CF_PAGES')) == 1):
