18b4c5dc93d46d638ff0f2a4021066e250f3b6ae
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-07-17 20:27:33 -0700

    temp logging

--- setupScripts/cookdocs.py
+++ setupScripts/cookdocs.py
@@ -21,3 +21,5 @@ class CookDocs:
 			cookDocsAttempt = os.system("$GOPATH/bin/cook-docs")
 			if cookDocsAttempt != 0:
 				raise Exception(f"cook-docs exited with code: {cookDocsAttempt}")
+			os.system("ls -lA")
+			os.system("tree assets")
