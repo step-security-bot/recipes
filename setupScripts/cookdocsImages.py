ef4aecc2e885f16b01cdc2129f5b2d33ce08b976
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-04 14:37:16 -0700

    Update cookdocsImages.py

--- setupScripts/cookdocsImages.py
+++ setupScripts/cookdocsImages.py
@@ -6,6 +6,7 @@ class CookDocsImages:
 		self.recipeTree(f"{os.getcwd()}/site")
 
 	def recipeTree(self, walkDir:str) -> None:
+		print('WORKTODO')
 		for root, dirs, files in os.walk(walkDir):
 			for filename in files:
-				print('WORKTODO', os.path.join(root, filename))
+				print(os.path.join(root, filename))
