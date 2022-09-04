0577abb8bdb38b191eaaa83cc2234a6ab6bd99d2
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-04 15:26:44 -0700

    Begin move asset to right location

--- setupScripts/cookdocsImages.py
+++ setupScripts/cookdocsImages.py
@@ -1,5 +1,6 @@
 import os
 import re
+import shutil
 
 class CookDocsImages:
 	def __init__(self) -> None:
@@ -10,5 +11,20 @@ class CookDocsImages:
 		for root, dirs, files in os.walk(walkDir):
 			for filename in files:
 				if re.search(r'^((?!icon).)+\.(jpg|png)$', filename, flags=re.IGNORECASE):
-					print(os.path.join(root, filename))
-					# print(filename)
+					self.moveAsset(root, filename)
+	
+	def standardizeName(self, filename) -> str:
+		# Matching https://github.com/nicholaswilde/cook-docs/blob/main/pkg/document/template.go#L50
+		noSpaces = re.sub("\s", "-", filename)
+		lowercase = noSpaces.lower()
+		return lowercase
+	
+	def moveAsset(self, root, filename) -> None:
+		# Check if assets folder exists
+		temp = os.path.join(root, 'assets')
+		print(temp)
+		if os.path.isdir(temp):
+			print('assets exists')
+		else:
+			print('assets doesn't exist')
+		# shutil.move(os.path.join(root, filename), os.path.join(root, 'assets/images', self.standardizeName(filename)))
