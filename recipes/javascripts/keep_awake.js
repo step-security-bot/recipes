d31733a1eab971381babe9673062d73fb0ff3d53
Victor <1209494+demosjarco@users.noreply.github.com>
2022-09-05 03:27:22 -0700

    Back to classes

--- recipes/javascripts/keep_awake.js
+++ recipes/javascripts/keep_awake.js
@@ -1,23 +1,27 @@
 "use strict";
 // https://www.educative.io/answers/how-to-keep-your-screen-awake-using-javascript
-if ("wakeLock" in navigator) {
-	createButton();
-}
+class KeepAwake {
+	#checkboxSelector = "header nav form.md-header__option input#keepAwake";
+	constructor() {
+		if ("wakeLock" in navigator) {
+			this.#createButton();
+		}
+	}
 
-const checkboxSelector = "header nav form.md-header__option input#keepAwake";
-
-function createButton() {
-	$(() => {
-		$(`<form class="md-header__option">
-			<input id="keepAwake" type="checkbox" />
-			<label for="keepAwake">Keep Awake</label>
-		</form>`).insertBefore($("header nav .md-header__option").first());
-		$(checkboxSelector).change(() => {
-			const checked1 = $(this).is(":checked");
-			const checked2 = $(this).prop("checked");
-			const checked3 = $(checkboxSelector).is(":checked");
-			const checked4 = $(checkboxSelector).prop("checked");
-			console.log(checked1, checked2, checked3, checked4);
+	#createButton() {
+		$(() => {
+			$(`<form class="md-header__option">
+				<input id="keepAwake" type="checkbox" />
+				<label for="keepAwake">Keep Awake</label>
+			</form>`).insertBefore($("header nav .md-header__option").first());
+			$(this.#checkboxSelector).change(() => {
+				const ischecked = $(this.#checkboxSelector).is(":checked");
+				console.log(ischecked);
+			});
 		});
-	});
+	}
+
+	#buttonToggle() {}
 }
+
+new KeepAwake();
