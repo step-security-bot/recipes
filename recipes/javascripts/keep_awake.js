b5c4e9f0be423c403adbffe5606ba4204bb92dc7
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-05 01:43:56 -0700

    Update keep_awake.js

--- recipes/javascripts/keep_awake.js
+++ recipes/javascripts/keep_awake.js
@@ -4,15 +4,10 @@ class KeepAwake {
 	constructor() {
 		if ("wakeLock" in navigator) {
 			$(function () {
-				console.log("wake lock available");
 				$(`<form>
 					<input id="keepAwake" type="checkbox" />
 					<label for="keepAwake">Keep Awake</label>
 				</form>`).insertBefore($("header nav .md-header__option").first());
-			});
-		} else {
-			$(function () {
-				console.log("wake lock unavailable");
 			});
 		}
 	}
