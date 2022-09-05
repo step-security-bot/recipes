14db4f2b81dc0ca44e4192cb687fcba9256ad8c8
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-05 00:36:21 -0700

    jquery available test

--- recipes/javascripts/keep_awake.js
+++ recipes/javascripts/keep_awake.js
@@ -1,3 +1,5 @@
 "use strict";
 // https://www.educative.io/answers/how-to-keep-your-screen-awake-using-javascript
-console.log('keep awake test');
+$(function () {
+	console.log("keep awake jquery test");
+});
