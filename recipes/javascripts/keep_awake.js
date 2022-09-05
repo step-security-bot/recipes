596638bbf7e349210518833da67083a2eec67a3d
Victor <1209494+demosjarco@users.noreply.github.com>
2022-09-05 03:13:27 -0700

    More accessible

--- recipes/javascripts/keep_awake.js
+++ recipes/javascripts/keep_awake.js
@@ -4,13 +4,15 @@ if ("wakeLock" in navigator) {
 	createButton();
 }
 
+const checkboxSelector = "header nav form.md-header__option input#keepAwake";
+
 function createButton() {
 	$(() => {
 		$(`<form class="md-header__option">
 			<input id="keepAwake" type="checkbox" />
 			<label for="keepAwake">Keep Awake</label>
 		</form>`).insertBefore($("header nav .md-header__option").first());
-		$('header nav form.md-header__option input#keepAwake').change(() => {
+		$(checkboxSelector).change(() => {
 			const ischecked = $(this).is(":checked");
 			console.log(ischecked);
 		});
