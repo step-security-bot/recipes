c61953d437645ff49bd57126fd1d912ca8dff1af
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-12 18:10:16 -0700

    Update cookDocs.py

--- setupScripts/cookDocs.py
+++ setupScripts/cookDocs.py
@@ -7,7 +7,7 @@ class CookDocs:
 		self.run()
 
 	def download(self) -> None:
-		testdebug = subprocess.run(["go", "install", "github.com/nicholaswilde/cook-docs/cmd/cook-docs@latest"], capture_output=True, check=True, text=True)
+		testdebug = subprocess.run(["go", "install", "github.com/nicholaswilde/cook-docs/cmd/cook-docs@latest"], stdout=subprocess.PIPE, check=True, text=True)
 		print(testdebug)
 
 	def run(self) -> None:
