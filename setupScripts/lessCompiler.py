45dcc57fb9119f428dbfb67adba50e901a4823cf
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-12 17:09:03 -0700

    Update lessCompiler.py

--- setupScripts/lessCompiler.py
+++ setupScripts/lessCompiler.py
@@ -7,7 +7,3 @@ class LessCompiler:
 
 	def runLesscpy(self) -> None:
 		subprocess.run(["npm", "run", "less"], capture_output=True, check=True)
-		# print('running:', f"lesscpy -t -r -g -V {os.getcwd()}/recipes/stylesheets/", flush=True)
-		# lesscpyAttempt = os.system(f"lesscpy -t -r -g -V {os.getcwd()}/recipes/stylesheets/")
-		# if lesscpyAttempt != 0:
-			# raise Exception(f"lesscpy exited with code: {lesscpyAttempt}")
