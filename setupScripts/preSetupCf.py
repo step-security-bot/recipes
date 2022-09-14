f04e5b106ebd2574e7603dfee43b3563e4c5e246
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-14 12:17:31 -0700

    Use os.system for debugging

--- setupScripts/preSetupCf.py
+++ setupScripts/preSetupCf.py
@@ -1,16 +1,18 @@
 import subprocess
+import os
 
 class PreSetupCF:
 	def __init__(self, isSudo) -> None:
 		self.isSudo = isSudo
 
 	def installMkDocsDeps(self) -> None:
-		commands = ["apt", "install", "-y", "libcairo2-dev", "libfreetype6-dev", "libffi-dev", "libjpeg-dev", "libpng-dev", "libz-dev"]
-		if not self.isSudo:
-			commands.insert(0, "sudo")
-		aptInstallAttempt = subprocess.run(commands, capture_output=True, check=True, text=True)
-		print(aptInstallAttempt.stdout, flush=True)
-		print(aptInstallAttempt.stderr, flush=True)
+		# commands = ["apt", "install", "-y", "libcairo2-dev", "libfreetype6-dev", "libffi-dev", "libjpeg-dev", "libpng-dev", "libz-dev"]
+		# if not self.isSudo:
+		# 	commands.insert(0, "sudo")
+		# aptInstallAttempt = subprocess.run(commands, capture_output=True, check=True, text=True)
+		# print(aptInstallAttempt.stdout, flush=True)
+		# print(aptInstallAttempt.stderr, flush=True)
+		os.system(f'{self.isSudo == True and "" or "sudo"} apt install -y libcairo2-dev libfreetype6-dev libffi-dev libjpeg-dev libpng-dev libz-dev')
 
 	def npmCi(self, production:bool = False) -> None:
 		npmCiAttempt = subprocess.run(["npm", "ci", f"--production={production == True and 'true' or 'false'}"], capture_output=True, check=True, text=True)
