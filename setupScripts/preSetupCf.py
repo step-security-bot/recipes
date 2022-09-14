55c7f94fbbfca816dc3fb44ffc22752f164c955a
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-14 13:11:03 -0700

    Remove debug logging

--- setupScripts/preSetupCf.py
+++ setupScripts/preSetupCf.py
@@ -6,12 +6,8 @@ class PreSetupCF:
 		self.isSudo = isSudo
 
 	def installMkDocsDeps(self) -> None:
-		testCommand1 = f'{self.isSudo == True and "" or "sudo"} apt update'
-		print('RUNNING:', testCommand1)
-		os.system(testCommand1)
-		testCommand2 = f'{self.isSudo == True and "" or "sudo"} apt install -y libcairo2-dev libfreetype6-dev libffi-dev libjpeg-dev libpng-dev libz-dev'
-		print('RUNNING:', testCommand2)
-		os.system(testCommand2)
+		os.system(f'{self.isSudo == True and "" or "sudo"} apt update')
+		os.system(f'{self.isSudo == True and "" or "sudo"} apt install -y libcairo2-dev libfreetype6-dev libffi-dev libjpeg-dev libpng-dev libz-dev')
 	def npmCi(self, production:bool = False) -> None:
 		npmCiAttempt = subprocess.run(["npm", "ci", f"--production={production == True and 'true' or 'false'}"], capture_output=True, check=True, text=True)
 		print(npmCiAttempt.stdout, flush=True)
