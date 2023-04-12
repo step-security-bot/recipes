cc041bdc31ffc5c9d149bc2b490aa4881f6a79b2
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-04-12 16:00:48 -0700

    Ran prettier

--- recipes/javascripts/keepAwake.js
+++ recipes/javascripts/keepAwake.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
 // https://www.educative.io/answers/how-to-keep-your-screen-awake-using-javascript
 class KeepAwake {
-	#checkboxSelector = "header nav form.md-header__option input#keepAwake";
+	#checkboxSelector = 'header nav form.md-header__option input#keepAwake';
 	#screenLock = null;
 
 	constructor() {
 		// Check feature availability
-		if ("wakeLock" in navigator) {
+		if ('wakeLock' in navigator) {
 			this.#createButton();
 		}
 	}
@@ -18,11 +18,11 @@ class KeepAwake {
 			$(`<form class="md-header__option">
 				<input id="keepAwake" type="checkbox" />
 				<label for="keepAwake">Keep Awake</label>
-			</form>`).insertBefore($("header nav .md-header__option").first());
+			</form>`).insertBefore($('header nav .md-header__option').first());
 			// On checkbox change
 			$(this.#checkboxSelector).change(() => {
 				$(() => {
-					const ischecked = $(this.#checkboxSelector).prop("checked");
+					const ischecked = $(this.#checkboxSelector).prop('checked');
 					if (ischecked) {
 						this.#lockScreen();
 					} else {
@@ -33,8 +33,8 @@ class KeepAwake {
 		});
 		// Browsers release wake lock on loss of visibility, low battery, etc
 		// if screen lock was previously on, set it again
-		$(document).on("visibilitychange", () => {
-			if (this.#screenLock !== null && document.visibilityState === "visible") {
+		$(document).on('visibilitychange', () => {
+			if (this.#screenLock !== null && document.visibilityState === 'visible') {
 				this.#lockScreen();
 			}
 		});
@@ -43,34 +43,34 @@ class KeepAwake {
 	#lockScreen() {
 		// Attempt to wake lock
 		try {
-			navigator.wakeLock.request("screen").then((lock) => {
+			navigator.wakeLock.request('screen').then((lock) => {
 				this.#screenLock = lock;
 				$(() => {
-					$(this.#checkboxSelector).prop("checked", true);
+					$(this.#checkboxSelector).prop('checked', true);
 				});
 				// call back when system auto releases wake lock
 				this.#screenLock.onrelease = () => {
 					$(() => {
-						$(this.#checkboxSelector).prop("checked", !this.#screenLock.released);
+						$(this.#checkboxSelector).prop('checked', !this.#screenLock.released);
 						if (this.#screenLock.released) {
 							this.#screenLock = null;
 						}
 					});
 				};
 			});
-		// Denied wake lock
+			// Denied wake lock
 		} catch (error) {
 			console.error(error);
 			this.#screenLock = null;
 			$(() => {
-				$(this.#checkboxSelector).prop("checked", false);
+				$(this.#checkboxSelector).prop('checked', false);
 			});
 		}
 	}
 
 	#unlockScreen() {
 		// Make sure wake lock still retained
-		if (typeof this.#screenLock !== "undefined" && this.#screenLock != null) {
+		if (typeof this.#screenLock !== 'undefined' && this.#screenLock != null) {
 			// Release wake lock
 			this.#screenLock.release();
 		}
