68f7908f0e148a5b649ee5cb195bdcf7a3f57f14
Victor <1209494+demosjarco@users.noreply.github.com>
2023-07-09 09:21:31 -0700

    More debug

--- hooks/cookDocs.py
+++ hooks/cookDocs.py
@@ -18,17 +18,23 @@ class CookDocs:
 		elif (getenv('CF_PAGES') != None and int(getenv('CF_PAGES')) == 1):
 			gopath = '$GOPATH/bin/'
 
-		ls_attempt = run(f"ls -lia $GOBIN", capture_output=True, shell=True, check=True, text=True)
+		print("Before $GOBIN")
+		ls_attempt = run(f"ls -lia $GOBIN", capture_output=True, shell=True, check=False, text=True)
 		print(ls_attempt.stdout, flush=True)
 		print(ls_attempt.stderr, flush=True)
+		print("After $GOBIN")
 
-		ls_attempt2 = run(f"ls -lia $GOPATH/bin", capture_output=True, shell=True, check=True, 	text=True)
+		print("Before $GOPATH/bin")
+		ls_attempt2 = run(f"ls -lia $GOPATH/bin", capture_output=True, shell=True, check=False, 	text=True)
 		print(ls_attempt2.stdout, flush=True)
 		print(ls_attempt2.stderr, flush=True)
+		print("After $GOPATH/bin")
 
-		ls_attempt3 = run(f"ls -lia $HOME/go/bin", capture_output=True, shell=True, check=True, 	text=True)
+		print("Before $HOME/go/bin")
+		ls_attempt3 = run(f"ls -lia $HOME/go/bin", capture_output=True, shell=True, check=False, 	text=True)
 		print(ls_attempt3.stdout, flush=True)
 		print(ls_attempt3.stderr, flush=True)
+		print("After $HOME/go/bin")
 
 		cookDocsAttempt = run([f"{gopath}cook-docs"], capture_output=True, shell=True, check=True, 	text=True)
 		print(cookDocsAttempt.stdout, flush=True)
