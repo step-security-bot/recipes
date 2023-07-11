cacac8e7c3156843c4b37a6e9f2373cdc23b867c
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-07-11 16:00:45 -0700

    Remove debug logging

--- hooks/cookDocs.py
+++ hooks/cookDocs.py
@@ -20,45 +20,7 @@ class CookDocs:
 		print(goInstallAttempt.stderr, flush=True)
 
 	def run(self) -> None:
-		if (getenv('GITHUB_ACTIONS') != None and bool(getenv('GITHUB_ACTIONS')) == True):
-			gopath = ''
-		elif (getenv('CF_PAGES') != None and int(getenv('CF_PAGES')) == 1):
-			gopath = ''
-
-		ls1 = '$GOBIN'
-		print(f"Before {ls1}")
-		ls_attempt = run(f"ls -lia {ls1}", capture_output=True, shell=True, check=False, text=True)
-		print(ls_attempt.stdout, flush=True)
-		print(ls_attempt.stderr, flush=True)
-		print(f"After {ls1}")
-
-		print(f"Before {gopath}")
-		ls_attempt2 = run(f"ls -lia {gopath}", capture_output=True, shell=True, check=False, text=True)
-		print(ls_attempt2.stdout, flush=True)
-		print(ls_attempt2.stderr, flush=True)
-		print(f"After {gopath}")
-
-		ls3 = '$HOME/go/bin'
-		print(f"Before {ls3}")
-		whereIs_attempt = run(f"ls -lia {ls3}", capture_output=True, shell=True, check=False, text=True)
-		print(whereIs_attempt.stdout, flush=True)
-		print(whereIs_attempt.stderr, flush=True)
-		print(f"After {ls3}")
-
-		whereIs = 'cook-docs'
-		print(f"Before {whereIs}")
-		whereIs_attempt = run(f"whereis {whereIs}", capture_output=True, shell=True, check=False, text=True)
-		print(whereIs_attempt.stdout, flush=True)
-		print(whereIs_attempt.stderr, flush=True)
-		print(f"After {whereIs}")
-
-		print(f"Before find {whereIs}")
-		whereIs_attempt = run(f"find / -iname {whereIs}", capture_output=True, shell=True, check=False, text=True)
-		print(whereIs_attempt.stdout, flush=True)
-		print(whereIs_attempt.stderr, flush=True)
-		print(f"After find {whereIs}")
-
-		cookDocsAttempt = run([f"{gopath}cook-docs"], capture_output=True, shell=True, check=True, text=True)
+		cookDocsAttempt = run(["cook-docs"], capture_output=True, shell=True, check=True, text=True)
 		print(cookDocsAttempt.stdout, flush=True)
 		print(cookDocsAttempt.stderr, flush=True)
 
