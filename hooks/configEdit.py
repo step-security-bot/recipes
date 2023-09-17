096b2b0b4ef4c71f49f081cc469fa469bdfc1849
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-09-17 00:53:10 -0700

    Add link back to main site

--- hooks/configEdit.py
+++ hooks/configEdit.py
@@ -1,4 +1,11 @@
 from mkdocs.config.base import Config
+from mkdocs.structure.nav import Navigation, Section, Link
+from mkdocs.structure.files import Files
 
 def on_config(config: Config) -> Config:
 	pass
+
+def on_nav(nav: Navigation, config: Config, files: Files) -> Navigation | None:
+	nav.items.insert(0, Section(title="Main Site", children=[Link(title=None, url="https://demosjarco.dev")]))
+
+	return nav
