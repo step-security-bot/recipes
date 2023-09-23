79ae5949a7e5a95ec4b5be67678c3298d64c5cb2
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-09-23 08:53:19 -0700

    Hash check before compiling
    Fixes `mkdocs serve` infinite reload

--- hooks/lessCompiler.py
+++ hooks/lessCompiler.py
@@ -1,17 +1,33 @@
+from hashlib import sha512
 from mkdocs.config.defaults import MkDocsConfig
 from pathlib import Path
 from io import StringIO
 import lesscpy
 
 class LessCompiler:
+	@staticmethod
+	def get_sha512_hash(content: str) -> str:
+		return sha512(content.encode()).hexdigest()
+
+	@staticmethod
 	def compileCSS(cssFilePath: Path, lessFilePath: Path) -> None:
 		with open(lessFilePath, 'r') as lessFile:
 			lessFileText = lessFile.read()
 
-		cssFileText = lesscpy.compile(StringIO(lessFileText), tabs=True, spaces=False)
-		with open(cssFilePath, 'w') as cssFile:
-			cssFile.write(cssFileText)
-		print(f"Compiled {lessFilePath} to {cssFilePath}", flush=True)
+		newCssFileText = lesscpy.compile(StringIO(lessFileText), tabs=True, spaces=False)
+
+		try:
+			with open(cssFilePath, 'r', encoding='utf-8') as cssFile:
+				oldCssFileText = cssFile.read()
+		except FileNotFoundError:
+			oldCssFileText = ""
+
+		if LessCompiler.get_sha512_hash(newCssFileText) != LessCompiler.get_sha512_hash(oldCssFileText):
+			with open(cssFilePath, 'w') as cssFile:
+				cssFile.write(newCssFileText)
+			print(f"Compiled {lessFilePath} to {cssFilePath}", flush=True)
+		else:
+			print(f"No changes detected; skipping compilation for {lessFilePath}", flush=True)
 
 # Run `on_config` because it runs before `get_files`
 def on_config(config: MkDocsConfig) -> MkDocsConfig | None:
